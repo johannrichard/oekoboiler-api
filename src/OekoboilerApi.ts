6450ab0e63231917f48008a537511f6e4cde5dc8
Johann Richard <189003+johannrichard@users.noreply.github.com>
2023-01-05 20:11:09 +0100

    docs: 📝 add main class and method documentation

--- src/OekoboilerApi.ts
+++ src/OekoboilerApi.ts
@@ -14,23 +14,60 @@ export class OekoboilerApi {
 
   protected aylaNetworkService: AylaDeviceApiService;
 
+  /**
+   * Main Oekboiler Api Class
+   *
+   * implements an Oekoboiler Api client, connects
+   * to the AylaNetwork Services (EU) and wraps it into
+   *
+   *
+   * @param userEmail: your Oekoboiler App username
+   * @param userPassword: your Oekoboiler App password
+   */
   constructor(userEmail: string, userPassword: string) {
     this.aylaNetworkService = new AylaDeviceApiService(userEmail, userPassword);
   }
 
+  /**
+   * Get Oekoboiler devices
+   *
+   * @returns Oekoboiler devices
+   */
   public getDevices(): Promise<Device[]> {
     return this.aylaNetworkService.getDevices();
   }
 
+  /**
+   *
+   * @param key property key (See {@link Device})
+   * @param value
+   * @returns
+   */
   public async updateProperty(key: number, value: unknown) {
     return this.aylaNetworkService.updateDeviceProperty(key, value);
   }
 
+  /**
+   * Get Oekoboiler Data mapped into {@link OekoboilerDevice}
+   *
+   * @param dsn Boiler DSN
+   * @returns an Oekoboiler properties type
+   */
   public async getBoiler(dsn: string): Promise<OekoboilerDevice> {
     const properties = await this.aylaNetworkService.getDeviceProperties(dsn);
     return this.getBoilerFromProperties(properties);
   }
 
+  /**
+   * Properties helper function
+   *
+   * @remarks
+   * Returns key:value pair for a specific, named property
+   *
+   * @param name device property name
+   * @param properties device properties
+   * @returns device property key:value pair
+   */
   private getPropertyKeyAndValue = (
     name: string,
     properties: DeviceProperty[],
@@ -48,6 +85,15 @@ export class OekoboilerApi {
     }
   };
 
+  /**
+   * Helper function
+   *
+   * @remarks
+   * folds the raw properties into a Oekboiler device
+   *
+   * @param properties array of a devices' raw properties
+   * @returns an Oekoboiler device
+   */
   private getBoilerFromProperties = (
     properties: DeviceProperty[],
   ): OekoboilerDevice => {
