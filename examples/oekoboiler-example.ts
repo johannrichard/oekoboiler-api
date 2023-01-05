3b9a33b0a9f82e000241365e083f679665899c67
Johann Richard <189003+johannrichard@users.noreply.github.com>
2023-01-05 17:10:15 +0100

    docs: 📝 sample integration

--- examples/oekoboiler-example.ts
+++ examples/oekoboiler-example.ts
@@ -1,15 +1,15 @@
 // Construct your API instance
-import OekoboilerApi from 'oekoboiler-api-client';
+import OekoboilerApi from 'oekoboiler-api';
 
 const oekoboiler = new OekoboilerApi(
   process.env.OB_USER_MAIL || '',
-  process.env.OB_USER_PASSWORD || ''
+  process.env.OB_USER_PASSWORD || '',
 );
 
 console.log(`Display Boiler ${process.env.OB_DSN}`);
 oekoboiler
   .getBoiler(process.env.OB_DSN || '')
-  .then(boiler => {
+  .then((boiler) => {
     console.info(boiler);
   })
   .then(() => {
@@ -19,10 +19,10 @@ oekoboiler
      */
 
     console.log('Iterating over all your Boilers');
-    oekoboiler.getDevices().then(devices => {
-      devices.forEach(boilerDevice => {
-        console.info((boilerDevice as unknown) as string);
-        oekoboiler.getBoiler(boilerDevice.device.dsn).then(boiler => {
+    oekoboiler.getDevices().then((devices) => {
+      devices.forEach((boilerDevice) => {
+        console.info(boilerDevice as unknown as string);
+        oekoboiler.getBoiler(boilerDevice.device.dsn).then((boiler) => {
           console.info(boiler);
         });
       });
